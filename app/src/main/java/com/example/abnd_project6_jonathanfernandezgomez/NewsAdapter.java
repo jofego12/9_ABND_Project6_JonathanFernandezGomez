65c2866db389290267fa4bdcbd58e5c05cd763b4
Jonathan Fernández Gómez <35399848+jofego12@users.noreply.github.com>
2019-06-11 15:40:38 -0700

    feat: Context

---
+++ app/src/main/java/com/example/abnd_project6_jonathanfernandezgomez/NewsAdapter.java
@@ -0,0 +1,31 @@
+package com.example.abnd_project6_jonathanfernandezgomez;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class NewsAdapter extends ArrayAdapter<News> {
+
+        public NewsAdapter(Context mainContext, ArrayList<News> news) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(mainContext, 0, news);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_list_items, parent, false);
+        }
+
+        return listItemView;
+    }
+}
